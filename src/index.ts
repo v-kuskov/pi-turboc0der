9c94af007bcdf6705da2b9c18806ba111d333d66
Vladimir Kuskov <v.kuskov@hotmail.com>
2026-05-18 00:19:22 +0500

    Register ls tool with agent
    - Added import of lsToolDef from ./tools/ls
    - Registered lsToolDef via pi.registerTool

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 import { TOOLS_PROMPT, SECURITY_PROMPT, IDENTITY_PROMPT, CAVEMAN_PROMPT, WORKFLOW_PROMPT, DESIGN_PROMPT, CODING_PROMPT, systemStatePrompt, INTERNAL_TOOLS } from "./prompts";
 import { rgToolDef } from "./tools/rg";
 import { fdToolDef } from "./tools/fd";
+import { lsToolDef } from "./tools/ls";
 
 export default function (pi: ExtensionAPI) {
     // Register custom rg and fd tools
     pi.registerTool(rgToolDef);
     pi.registerTool(fdToolDef);
+    pi.registerTool(lsToolDef);
 
     pi.on("before_agent_start", async (_event, _ctx) => {
         const prompt =
